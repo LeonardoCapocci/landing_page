71a4b86a2095a74640c15bed8886367a9d6e3803
LeonardoCapocci <leocap2001@gmail.com>
2023-10-31 12:43:33 -0700

    Added a for loop for photo adding

---
+++ script.js
@@ -0,0 +1,24 @@
+for (let i = 1; i <= 36; i++) {
+  // Create a new div element for each set of images
+  const imageContainer = document.createElement('div');
+  imageContainer.className = 'imageContainer';
+
+  // Create an image element
+  const img = document.createElement('img');
+  img.className = 'pic';
+  img.src = `images/${i}.jpeg`;
+  img.alt = '';
+
+  // Create a paragraph element
+  const p = document.createElement('p');
+  p.className = 'subtitle';
+  p.textContent = '';
+
+  // Append the image and paragraph to the bodyContainer
+  imageContainer.appendChild(img);
+  imageContainer.appendChild(p);
+
+  // Append the bodyContainer to the document
+  let bodyContainer = document.querySelector('.bodycontainer')
+  bodyContainer.appendChild(imageContainer);
+}
